44b9b3c91bf313503c689dfc8fae2bcfeb79d2a5
Renat <suleymanov_renat@mail.ru>
2020-03-05 23:46:13 +0300

    дописать тест, остальное сделано, возможно нужно переделать основную функцию, т.к. пока неясно как с ней тестить

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-let buy = document.getElementById("buy");
-let sumBuy = buy.textContent.trim();
-
-let button = document.getElementById("but");
-button = addEventListener("click", verificationSum);
-
-function verificationSum() {
-    console.log(sumBuy);
-}
+import "../src/add";
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: "./src/index.js",
     output: {
         filename: "app.boundle.js",
         path: path.resolve(__dirname, "dist")
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "index.html"
+        })
+    ],
 }
+
